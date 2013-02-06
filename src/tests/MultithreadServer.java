a1ac5f42f820d980dd5149b202bff15b8700a2a1
Jocelyn Batton <batton.jocelyn@gmail.com>
2013-02-06 23:59:31 +0100

    debut de multithreading pour gestion requete

---
+++ src/tests/MultithreadServer.java
@@ -0,0 +1,63 @@
+package tests;
+
+import java.io.IOException;
+import java.net.*;
+ 
+public class MultithreadServer {
+ 
+    public static void main(String[] zero){
+         
+        ServerSocket socket;
+        try {
+        socket = new ServerSocket(2009);
+        Thread t = new Thread(new Accepter_clients(socket, 1));
+        Thread t2 = new Thread(new Accepter_clients(socket, 2));
+        Thread t3 = new Thread(new Accepter_clients(socket, 3));
+        Thread t4 = new Thread(new Accepter_clients(socket, 4));
+        t.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        
+        System.out.println("Mes employeurs sont prêts !");
+         
+        } catch (IOException e) {
+             
+            e.printStackTrace();
+        }
+    }
+}
+ 
+class Accepter_clients implements Runnable {
+ 
+       private ServerSocket socketserver;
+       private Socket socket;
+       private int num;
+       private int nbrclient = 1;
+        public Accepter_clients(ServerSocket s, int num){
+            socketserver = s;
+            this.num = num;
+        }
+         
+        public void run() {
+ 
+            try {
+                while(true){
+              socket = socketserver.accept(); // Un client se connecte on l'accepte
+                      System.out.println("Le client numéro "+nbrclient+ " est connecté !" + " num = " + num);
+                      nbrclient++;
+                      socket.close();
+                      try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                }
+             
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+ 
+    }
